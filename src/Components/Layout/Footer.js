86518b190dbb76e6fa8770ded9ab65e36b017826
jnutterdev <hello@jnutterdev.com>
2021-02-09 18:20:20 -0500

    added in a lifecycle componentdidmount

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {favEmoji: "👍"};
+      }
+    componentDidMount() {
+        setTimeout(() => {
+          this.setState({favEmoji: "😎"})
+        }, 1000)
+    }
 
     render() {
 		return (
             <div className="footer">
-                &copy; 2021. All design, content, and music by jnutterdev.
+                &copy; 2021. All design, content, and music by jnutterdev. {this.state.favEmoji}
             </div>
         )
 }
